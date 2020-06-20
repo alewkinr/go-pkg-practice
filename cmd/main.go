e3fe8e32a13ee8a3678bbb73b254402ed83e433f
raleshkin <raleshkin@zigmund.online>
2020-06-20 20:35:25 +0300

    :bug: Fixing a bug.

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	 "github.com/alewkinr/go-modules-practice"
-	 "github.com/alewkinr/go-modules-practice/first-pkg"
+	"github.com/alewkinr/go-modules-practice"
+	"github.com/alewkinr/go-modules-practice/first-pkg"
 )
 
 func main() {
 	fmt.Println("It's main.go")
 	firstpkg.DirRun()
 	firstpkg.RootRun()
-
+}
